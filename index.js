db94b1892c2a7f1e84834178b8824dbfa1fdd1a8
yelloumi <yelloumi@ualberta.ca>
2025-03-13 15:25:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
 import { getDatabase,
          ref,
          push,
          onValue,
-         remove } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js"
+         remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-tracker-app-b2618-default-rtdb.firebaseio.com/"
